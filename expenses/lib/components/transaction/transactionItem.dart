c6791dc7ce45226f61acb0a7d27903c7b14d9585
MicaelLevi <micaellevins@gmail.com>
2021-01-19 10:31:48 -0300

    Codigo de item refatorado

---
+++ expenses/lib/components/transaction/transactionItem.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../models/transaction.dart';
+
+class TransactionItem extends StatelessWidget {
+  final Transaction tr;
+  final void Function(String) onDelete;
+
+  const TransactionItem({
+    Key key,
+    @required this.tr,
+    @required this.onDelete,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.symmetric(
+        vertical: 8,
+        horizontal: 5,
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          child: Padding(
+            padding: const EdgeInsets.all(6),
+            child: FittedBox(
+              child: Text('R\$${tr.value}'),
+            ),
+          ),
+        ),
+        title: Text(
+          tr.title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        subtitle: Text(
+          DateFormat('d MMM y').format(tr.date),
+        ),
+        trailing: MediaQuery.of(context).size.width > 480
+            ? FlatButton.icon(
+                onPressed: () => onDelete(tr.id),
+                icon: Icon(Icons.delete),
+                label: Text('Excluir'),
+                textColor: Theme.of(context).errorColor,
+              )
+            : IconButton(
+                icon: Icon(Icons.delete),
+                color: Theme.of(context).errorColor,
+                onPressed: () => onDelete(tr.id),
+              ),
+      ),
+    );
+  }
+}
