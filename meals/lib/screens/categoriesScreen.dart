5aec38119e95b807ef4be308c490860c6432e9af
MicaelLevi <micaellevins@gmail.com>
2021-01-25 17:15:22 -0300

    Adicionado fontes e dados no construtor

--- meals/lib/screens/categoriesScreen.dart
+++ meals/lib/screens/categoriesScreen.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
 
+import '../components/categoryItem.dart';
+import '../data/dummyData.dart';
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisExtent: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos Cozinhar?'),
       ),
-      children: [],
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisExtent: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
     );
   }
 }
